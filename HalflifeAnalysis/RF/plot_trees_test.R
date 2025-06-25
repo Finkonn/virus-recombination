54ccc02b5a4e5ea2e2f3b469bd6c83c3adede1c0
Finkonn <oromas01@gmail.com>
2025-06-25 13:39:16 +0300

    Add RF halflife tree visualisation and update tables with RF halflifes

--- HalflifeAnalysis/RF/plot_trees_test.R
+++ HalflifeAnalysis/RF/plot_trees_test.R
@@ -5,7 +5,7 @@ library(tidyr)
 library(ggpubr)
 
 # set  working directory
-#setwd("")
+setwd("C:/Users/Mate_/Downloads/compare_trees_RF")
 # Load functions from source code
 source("get_subtrees.R")
 
@@ -18,13 +18,13 @@ source("get_subtrees.R")
 # this file is output of get_RF_halflife.py
 # Example: python.exe .\get_RF_halflife.py -tree1 .\norovirus_vp1.tree -tree2 .\norovirus_rdrp_g2.tree
 
-subtrees = get_subtrees("result/O_bayes_lognormal_400mil_limited-O_O0.fasta_commontrees.txt")
+subtrees = get_subtrees("Asia1_Lpro/Asia1_P1_Asia1.fasta_commontrees.txt")
 
 # Path to file with tree in nexus format produced by BEAST
-tree1_name = "O_bayes_lognormal_400mil_limited-O.tree"
+tree1_name = "MCMC/Asia1_P1.tree"
 tree1 = read.beast(tree1_name)
 
-tree2_name = "O0.fasta.treefile"
+tree2_name = "Lpro/Asia1.fasta.treefile"
 tree2 = read.tree(tree2_name)
 
 # Plot beast subtree and color braches of common subtrees
@@ -34,7 +34,7 @@ p1 = groupOTU(p1, subtrees, 'Clade') + aes(color=Clade) +
   theme(legend.position="right") + scale_color_manual(values=c("black", "firebrick"))
 
 p2 = ggtree(tree2,mrsd="2025-01-01") +  geom_tiplab(size=2) + 
-            geom_label2(aes(label=round(as.numeric(posterior),2)),size=2)
+            geom_label2(aes(label=round(as.numeric(label),2)),size=2)
 p2 = groupOTU(p2, subtrees, 'Clade') + aes(color=Clade) +
   theme(legend.position="right") + scale_color_manual(values=c("black", "firebrick"))
 
@@ -42,43 +42,36 @@ ggarrange(p1,p2)
 
 # Example 2. Trees with the same leaf sets
 
-
 # Path to file with tree in nexus format produced by BEAST
-tree1_name = "O_bayes_lognormal_400mil_limited-O.tree"
+tree1_name = "MCMC/Asia1_P1.tree"
 tree1 = read.beast(tree1_name)
 
-tree2_name = "O0.fasta.treefile"
+tree2_name = "P3/Asia1.fasta.treefile"
 tree2 = read.tree(tree2_name)
 
 # common subtrees determined from coinciding bipartitions in two trees
-subtrees = get_subtrees("result/O_bayes_lognormal_400mil_limited-O_O0.fasta_commontrees.txt")
+subtrees = get_subtrees("Asia1_P3/Asia1_P1_Asia1.fasta_commontrees.txt")
 
-
-# Plot BEAST subtree and color branches of common subtrees
-p1 = ggtree(tree1, mrsd = "2025-01-01") + 
-  geom_tiplab(size = 1) + 
-  geom_nodepoint(aes(label = posterior, 
-                     subset = !is.na(as.numeric(posterior)) & as.numeric(posterior) >= 80),
-                 size = 1, color = "green") +  
+p1 = ggtree(tree1, mrsd = "2025-01-01", size = 0.3) + geom_tiplab(size = 1) + 
+  geom_point2(aes(subset = !is.na(as.numeric(posterior)) & as.numeric(posterior) > 0.9), 
+              shape = 21, fill = "blue", color = "black", size = 0.5) +
   theme_tree2()
 
-p1 = groupOTU(p1, subtrees, 'Clade') + 
-  aes(color = Clade) +
-  scale_color_manual(values = c("black", "firebrick")) +
-  theme(legend.position = "none")  # <-- убираем легенду
+p1 = groupOTU(p1, subtrees, 'Clade') + aes(color = Clade) +
+  scale_color_manual(values = c("black", "firebrick")) + 
+  coord_cartesian(clip = "off") +
+  theme_tree2(plot.margin = margin(5, 60, 5, 5), legend.position = "none")
 
-p2 = ggtree(tree2) + 
+p2 = ggtree(tree2, size = 0.3) +  
   geom_tiplab(size = 1) + 
-  geom_nodepoint(aes(label = label, 
-                     subset = !is.na(as.numeric(label)) & as.numeric(label) >= 100),
-                 size = 1, color = "green") +  
+  geom_point2(aes(subset = !is.na(as.numeric(label)) & as.numeric(label) > 90), 
+              shape = 21, fill = "blue", color = "black", size = 0.5) +
   theme_tree2()
 
-p2 = groupOTU(p2, subtrees, 'Clade') + 
-  aes(color = Clade) +
+p2 = groupOTU(p2, subtrees, 'Clade') + aes(color = Clade) +
   scale_color_manual(values = c("black", "firebrick")) +
-  theme(legend.position = "none")  # <-- убираем легенду
+  coord_cartesian(clip = "off") +
+  theme_tree2(plot.margin = margin(5, 60, 5, 5), legend.position = "none")
 
-# Combine plots
 ggarrange(p1, p2)
 
